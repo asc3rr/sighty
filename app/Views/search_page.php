dd64306b4b7863348697d92c151bacba58a42a9a
asc3rr <borysgnacinski@gmail.com>
2021-09-04 22:20:46 +0200

    made search_page compatible with main template, added form to search_age

--- app/Views/search_page.php
+++ app/Views/search_page.php
@@ -1,7 +1,16 @@
-<?= $this->extend("Views/templates/search_page"); ?>
-<?= $this->section("body"); ?>
+<?= $this->extend("templates/main"); ?>
 
-<div class="container">
-    <h1>Search bar</h1>
-</div>
+<?= $this->section("content"); ?>
+<h1 class="display-4 text-center"><?= $title ?></h1>
+<form class="text-center" action="result/" method="get">
+    <div class="form-group" id="latidute">
+        <label for="latitude">Latitude:</label>
+        <input id="latitude" name="latitude" type="text" pattern="^-?([0-8]?[0-9]|90)(\.[0-9]{1,10})$" placeholder="Latitude">
+    </div>
+    <div class="form-group" id="longitude">
+        <label for="longitude">Longitude:</label>
+        <input id="longitude" name="longitude" type="text" pattern="^-?([0-9]{1,2}|1[0-7][0-9]|180)(\.[0-9]{1,10})$" placeholder="Longitude">
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
 <?= $this->endSection(); ?>

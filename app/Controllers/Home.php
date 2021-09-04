ad8e28bc73cc0d84f55b6702e4302299abb67a0c
asc3rr <borysgnacinski@gmail.com>
2021-09-04 22:45:17 +0200

    added display data

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,14 @@ class Home extends BaseController
 {
 	public function index()
 	{
-		return view('search_page');
+		$data = [
+			"keywords" => "Random keyword",
+			"meta_title" => "Search Panel - Sighty",
+			"meta_description" => "Search Panel - Sighty",
+			"title" => "Search Panel",
+			"footer_content" => "&copy All rights reserved for asc3rr"
+		];
+
+		return view('search_page', $data);
 	}
 }

e25f8dbdf4390641bfed817ad6a0065e0bdd7d7a
asc3rr <borysgnacinski@gmail.com>
2021-09-04 22:04:58 +0200

    updated main template

--- app/Views/templates/main.php
+++ app/Views/templates/main.php
@@ -1,16 +1,55 @@
-<!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta name="keywords" content="<?= $keywords ?>">
+
+    <!-- <link rel="icon" href="src/img/logo.png"> -->
 
     <title><?= $meta_title ?></title>
     <meta name="description" content="<?= $meta_description ?>">
+    
+    <!-- Bootstrap section -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <!-- End Bootstrap section -->
+
+    <link rel="stylesheet" href="src/css/main.css" type="text/css">
 </head>
 <body>
-<?= $this->renderSection("body") ?>
-<footer>
-    <?= $footer_content ?>
-</footer>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-orange">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <?= $title ?>
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="dev/">Developers</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <main>
+        <div class="container">
+            <?= $this->renderSection("content"); ?>
+        </div>
+    </main>
+    <footer>
+        <?= $footer_content; ?>
+    </footer>
+    </div>
 </body>
 </html>

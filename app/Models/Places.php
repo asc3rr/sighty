69cd0da975c002e5f909595451d3ca99bbdb216c
asc3rr <borysgnacinski@gmail.com>
2021-09-05 16:51:29 +0200

    added Places model

---
+++ app/Models/Places.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class Places extends Model
+{
+    protected $table      = 'places';
+    protected $primaryKey = 'id';
+
+    protected $useAutoIncrement = true;
+
+    protected $returnType     = 'array';
+    protected $useSoftDeletes = false;
+
+    protected $allowedFields = ["latitude", "longitude", "place_name", "place_description", "author"];
+
+    protected $useTimestamps = true;
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    # protected $deletedField  = 'deleted_at';
+
+    # protected $validationRules    = [];
+    # protected $validationMessages = [];
+    # protected $skipValidation     = false;
+}

a3089dc1b6ac3d190c06202ac0c1ff4c9b1663f2
asc3rr <borysgnacinski@gmail.com>
2021-09-05 22:09:22 +0200

    changed variable from  to

--- app/Views/templates/main.php
+++ app/Views/templates/main.php
@@ -25,7 +25,7 @@
     <nav class="navbar navbar-expand-lg navbar-dark bg-orange">
         <div class="container">
             <a class="navbar-brand" href="#">
-                <?= $title ?>
+                <?= $meta_title ?>
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>

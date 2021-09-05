416225c477827ad0c473bbfb65a4c36ffc595db5
asc3rr <borysgnacinski@gmail.com>
2021-09-05 20:44:32 +0200

    fixed href value, was returning 404

--- app/Views/result.php
+++ app/Views/result.php
@@ -13,7 +13,7 @@ foreach($places as $place){
             <div class="card-body">
                 <h5 class="card-title">$place_name</h5>
                 <p class="card-text">$place_description</p>
-                <a href="/place?id=$place_id" class="btn btn-primary">See more</a>
+                <a href="/places?id=$place_id" class="btn btn-primary">See more</a>
             </div>
         </div>
     </div>

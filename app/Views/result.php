f6ee06c3b452a95b0c763007979adfaae38d5ea1
asc3rr <borysgnacinski@gmail.com>
2021-09-05 20:42:33 +0200

    finished(?) result view. it should work properly

--- app/Views/result.php
+++ app/Views/result.php
@@ -1,1 +1,26 @@
-dupa
+<?= $this->extend("templates/main"); ?>
+<?= $this->section("content"); ?>
+<div class="row">
+<?php
+foreach($places as $place){
+    $place_id = $place["id"];
+    $place_name = $place["place_name"];
+    $place_description = $place["place_description"];
+
+    echo <<<ENDL
+    <div class="col-sm-6">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">$place_name</h5>
+                <p class="card-text">$place_description</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    </div>
+    ENDL;
+}
+
+// TODO: sprawdzić więcej pozycji. Zmienić routing z `result`, dodać grupę `places` a w niej `places/result`
+?>
+</div>
+<?= $this->endSection(); ?>

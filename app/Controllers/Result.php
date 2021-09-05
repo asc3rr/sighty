beee707946caa5a90a62db47f288a1cf1c0e6ee0
asc3rr <borysgnacinski@gmail.com>
2021-09-05 16:52:35 +0200

    added result controller for places model

--- app/Controllers/Result.php
+++ app/Controllers/Result.php
@@ -2,12 +2,37 @@
 
 namespace App\Controllers;
 
+use App\Models\Places;
+
 class Result extends BaseController
 {
 	public function index()
 	{
-		
+        $latitude = $_GET["latitude"];
+        $longitude = $_GET["longitude"];
 
-		return view('result');
+        $places = $this->getPlaces($latitude, $longitude);
+
+        $data = [
+            "keywords" => "Random keyword",
+            "meta_title" => "Places - Sighty Results",
+            "meta_description" => "Places results for Lat: $latitude and Lng: $longitude",
+            "title" => "Places - Sighty Results",
+            "footer_content" => "&copy All rights reserved for asc3rr",
+            "places" => $places
+        ];
+
+		return view('result', $data);
 	}
+
+    public function getPlaces($latitude, $longitude){
+        $model = new Places();
+
+        $model->where("latitude", $latitude);
+        $model->where("longitude", $longitude);
+
+        $result = $model->findAll();
+
+        return $result;
+    }
 }

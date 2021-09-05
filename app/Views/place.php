89da6537a306fe389eee5eb88fca2047b5711ce2
asc3rr <borysgnacinski@gmail.com>
2021-09-05 22:05:10 +0200

    added place view

---
+++ app/Views/place.php
@@ -0,0 +1,9 @@
+<?= $this->extend("templates/main"); ?>
+<?= $this->section("content"); ?>
+<h3 class="display-3 text-center"><?= $title ?></h3>
+<h5 class="display-5 text-center"><?= base64_decode($place["place_shortdesc"]); ?></h5>
+
+<p class="display-6 place-desc text-break">
+    <?= base64_decode($place["place_description"]); ?>
+</p>
+<?= $this->endSection(); ?>

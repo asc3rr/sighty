caa257fc8226d8e739ef58ec647aabce2d594d4e
asc3rr <borysgnacinski@gmail.com>
2021-09-06 16:52:09 +0200

    added  class

--- app/Views/place/place.php
+++ app/Views/place/place.php
@@ -3,7 +3,7 @@
 <h3 class="display-3 text-center"><?= $title ?></h3>
 <h5 class="display-5 text-center"><?= base64_decode($place["place_shortdesc"]); ?></h5>
 
-<p class="display-6 place-desc rounded text-break">
+<p class="display-6 place-desc rounded text-wrap text-break">
     <?= base64_decode($place["place_description"]); ?>
 </p>
 <?= $this->endSection(); ?>

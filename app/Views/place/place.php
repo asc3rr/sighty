ad040dc018e98e57a526be06c95c3b75c445c50c
asc3rr <borysgnacinski@gmail.com>
2021-09-25 15:34:18 +0200

    added text-justify class to the description div

--- app/Views/place/place.php
+++ app/Views/place/place.php
@@ -2,7 +2,7 @@
 <?= $this->section("content"); ?>
 <h5 class="display-5 text-center"><?= base64_decode($place["place_shortdesc"]); ?></h5>
 
-<p class="display-6 place-desc rounded text-wrap text-break">
+<p class="display-6 place-desc rounded text-wrap text-break text-justify">
     <?= base64_decode($place["place_description"]); ?>
 </p>
 
